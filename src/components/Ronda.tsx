ff59538263f0713c02f45d33ded344dcd0c96f4d
Felipe <antoniofelipesilva@live.com>
2022-12-18 22:11:26 -0300

    version.1.0.9

--- src/components/Ronda.tsx
+++ src/components/Ronda.tsx
@@ -3,19 +3,24 @@ import { useState} from 'react';
 export const Honda = () => {
   const [HondaIniciadas, setHonda] = useState(0);
   const [HondaConcluidas, setHondaConcluidas] = useState(0);
+  const [libera, setLibera] = useState(false)
 
   const HondaIniciada = () => {
     setHonda(HondaIniciadas + 1)
+    handleClickHonda()
   }
   const HondaConcluida = () => {
     setHondaConcluidas(HondaConcluidas + 1)
+  }
+  const handleClickHonda = () => {
+    setLibera(true)
   }
   return(
     <div>
       <h2>Rondas Iniciadas {HondaIniciadas}</h2>
       <h2>Rondas Concluidas {HondaConcluidas}</h2>
       <button onClick={HondaIniciada}>Inicia Ronda</button>
-      <button onClick={HondaConcluida}>conclui Ronda</button>
+       {libera && <button onClick={HondaConcluida}>conclui Ronda</button> }
     </div>
   )
 }
