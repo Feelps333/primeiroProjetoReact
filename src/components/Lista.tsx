f5df7068edf327daba50bf53f0c74641c7cd0f20
Felipe <antoniofelipesilva@live.com>
2022-12-18 21:12:48 -0300

    version.1.0.6

--- src/components/Lista.tsx
+++ src/components/Lista.tsx
@@ -1,4 +1,11 @@
-export const List = () => {
+type Props = {
+  data: {
+    name: string;
+    age: number
+  }
+}
+
+export const List = ({ data }: Props) => {
   let list = [
     'felipe',
     'franca',
@@ -6,12 +13,22 @@ export const List = () => {
     'feelps',
     'antonio'
   ]
+  let listTwo = [
+    {name: 'cristiano', age: 25},
+    {name: 'messi', age: 47},
+    {name: 'ronaldo', age: 33},
+    {name: 'maldini', age: 20},
+    {name: 'mbappe', age: 53}
+  ]
 
 return (
   <div>
     <h2>Lista</h2>
     <ul>
       {list.map((item, index)=>(<li key={index}>{item.toLocaleUpperCase()}</li>))}
+    </ul>
+    <ul>
+      {listTwo.map((item, index)=>(<li key={index}>{item.name} - {item.age}</li>))}
     </ul>
   </div>
 )
